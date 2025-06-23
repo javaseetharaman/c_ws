10ea783272a0ffa0798879c01802243999ac360b
csalab1 <ssn.1972@kanchiuniv.ac.in>
2025-06-23 12:03:51 -0700

    fahrenheit to celcius added

--- temperature.c
+++ temperature.c
@@ -10,7 +10,13 @@ int main() {
 
   printf("%.2f Celsius = %.2f Fahrenheit\n", celsius, fahrenheit);
 
- 
+  printf("Now enter temperature in Fahrenheit: ");
+  scanf("%f", &fahrenheit);
+
+  celsius = (fahrenheit -32.0) * 5.0/9.0;
+
+  printf("%.2f Fahrenheit = %.2f Celsius\n",fahrenheit, celsius);
+
   return 0;
   
 }
